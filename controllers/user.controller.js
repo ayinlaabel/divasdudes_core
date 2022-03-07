deeb9c65d771d7f1d1e30f2255473b84845f9379
ayinlaabel <kraneabel@gmail.com>
2022-03-07 20:26:25 +0100

    Update

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -84,32 +84,32 @@ const userController = {
             })
             .catch((err) => console.log(err));
 
-          return user
-            .createSession()
-            .then((refreshToken) => {
-              //Session created successfully - refreshToken returned.
-              //now we generate an access auth token for the user.
+        //   return user
+        //     .createSession()
+        //     .then((refreshToken) => {
+        //       //Session created successfully - refreshToken returned.
+        //       //now we generate an access auth token for the user.
 
-              return user
-                .generateAccessAuthToken()
-                .then((accessToken) => {
-                  //access auth token generated successfully, now we return an object containing  the auth token
-                  return { accessToken, refreshToken };
-                })
-                .then((authToken) => {
-                  //Now we construct and send  the response to the user with their auth tokens in the header and the user object in the body
+        //       return user
+        //         .generateAccessAuthToken()
+        //         .then((accessToken) => {
+        //           //access auth token generated successfully, now we return an object containing  the auth token
+        //           return { accessToken, refreshToken };
+        //         })
+        //         .then((authToken) => {
+        //           //Now we construct and send  the response to the user with their auth tokens in the header and the user object in the body
 
-                  res
-                    .header("x-refresh-token", authToken.refreshToken)
-                    .header("x-access-token", authToken.accessToken)
-                    .send(user);
-                })
-                .catch((err) => {
-                  //   res.status(400).json(err);
-                  console.log(err);
-                });
-            })
-            .catch((err) => console.log(err));
+        //           res
+        //             .header("x-refresh-token", authToken.refreshToken)
+        //             .header("x-access-token", authToken.accessToken)
+        //             .send(user);
+        //         })
+        //         .catch((err) => {
+        //           //   res.status(400).json(err);
+        //           console.log(err);
+        //         });
+        //     })
+        //     .catch((err) => console.log(err));
         }
       })
       .catch((err) => {
